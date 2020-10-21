251f1ec500becf40bc8912fc2cfc1cd3514d8da1
Andrey Chernyakov <a.n.d@inbox.ru>
2020-10-21 16:49:05 +0300

    porting project to python 3

--- pg_export/render.py
+++ pg_export/render.py
@@ -3,7 +3,7 @@
 import os
 from jinja2 import Environment, FileSystemLoader
 from pg_export.acl import acl_to_grants
-from filters import untype_default, ljust, rjust, join_attr, concat_items
+from pg_export.filters import untype_default, ljust, rjust, join_attr, concat_items
 
 env = Environment(loader=FileSystemLoader('pg_export/templates'))
 
@@ -18,11 +18,11 @@ def render(template_name, context):
     try:
         res = env.get_template(template_name).render(context)
     except Exception as e:
-        print "Error on render template:", template_name
+        print("Error on render template:", template_name, file=sys.stderr)
         raise
     return res
 
 def render_to_file(template_name, context, file_name):
     if isinstance(file_name, tuple):
         file_name = os.path.join(*file_name)
-    open(file_name, 'a').write(render(template_name, context).encode('utf8'))
+    open(file_name, 'ab').write(render(template_name, context).encode('utf8'))
