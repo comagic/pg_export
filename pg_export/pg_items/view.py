a78d546febb29dc62a90a5bebe6154df5fb2347c
d.pashkov <d.pashkov@comagic.dev>
2022-03-26 00:02:54 +0300

    views on triggers

--- pg_export/pg_items/view.py
+++ pg_export/pg_items/view.py
@@ -1,4 +1,5 @@
 from pg_export.pg_items.item import Item
+from pg_export.filters import get_full_name
 from pg_export.acl import acl_to_grants
 
 
@@ -10,6 +11,7 @@ class View (Item):
         super(View, self).__init__(src, version)
         self.grants = acl_to_grants(self.acl, 'table', self.full_name)
         self.query = self.query[:-1]  # drop ";"
+        self.triggers = self.triggers or []
 
         if self.kind == 'm':
             self.directory = 'materializedviews'
@@ -19,3 +21,10 @@ class View (Item):
                                         'column',
                                         self.full_name,
                                         c['name'])
+
+        for t in self.triggers:
+            t['function'] = get_full_name(t['function_schema'],
+                                          t['function_name'])
+            if t['ftable_name']:
+                t['ftable'] = get_full_name(t['ftable_schema'],
+                                            t['ftable_name'])
