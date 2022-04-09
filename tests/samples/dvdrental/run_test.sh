8ebfb95c6bc0c808eb75ff49815d14cb85775588
Andrey Chernyakov <a.chernyakov@comagic.dev>
2022-04-09 14:14:57 +0300

    improve output

--- tests/samples/dvdrental/run_test.sh
+++ tests/samples/dvdrental/run_test.sh
@@ -6,16 +6,20 @@ export PGPASSWORD=postgres1723
 export VERSION=$1
 export PGPORT=1723
 
-echo
+echo run docker container: pg_export_test_$VERSION
 docker ps -q --filter "name=pg_export_test_$VERSION" | grep -q . && docker stop pg_export_test_$VERSION
 docker ps -qa --filter "name=pg_export_test_$VERSION" | grep -q . && docker rm pg_export_test_$VERSION
 docker run --name pg_export_test_$VERSION -p $PGPORT:5432 -e POSTGRES_PASSWORD=$PGPASSWORD -d postgres:$VERSION
 sleep 2
 
-psql -c "create database pg_export_test" postgres
+echo build sample db
+psql -c "create database pg_export_test" postgres > /dev/null
 psql -f source.sql pg_export_test > /dev/null
+echo export sample db
 pg_export --clean pg_export_test /tmp/pg_export_test
-diff -qr schemas /tmp/pg_export_test/schemas
+echo diff export with sample:
+diff -qr schemas /tmp/pg_export_test/schemas && echo test result: PASSED || echo test result: FAILED
 
-docker stop pg_export_test_$VERSION
-docker rm pg_export_test_$VERSION
+echo remove docker container
+docker stop pg_export_test_$VERSION > /dev/null
+docker rm pg_export_test_$VERSION > /dev/null
