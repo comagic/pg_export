a190a89b076fed72c25273f71f135aac3e315f04
Andrey Chernyakov <a.chernyakov@comagic.dev>
2025-01-20 21:33:45 +0300

    fix extra referencing

--- pg_export/templates/base/out/table.sql
+++ pg_export/templates/base/out/table.sql
@@ -109,8 +109,9 @@ create {%- if t.constraint %} constraint {%- endif %} trigger {{ t.name }}
   {%- if t.ftable %} from {{ t.ftable }} {%- endif %}
   {%- if t.deferrable %} deferrable {%- endif %}
   {%- if t.deferred %} initially deferred {%- endif %}
-  {%- if t.new_table %} referencing new table as {{ t.new_table }} {%- endif %}
-  {%- if t.old_table %} referencing old table as {{ t.old_table }} {%- endif %}
+  {%- if t.new_table or t.old_table %} referencing {%- endif %}
+  {%- if t.new_table %} new table as {{ t.new_table }} {%- endif %}
+  {%- if t.old_table %} old table as {{ t.old_table }} {%- endif %}
   for each {{ t.each }} {% if t.condition -%} when ({{ t.condition }}) {% endif -%}
   execute function {{ t.function }}({{ t.arguments }});
 {%- endfor %}
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.command.test import test as TestCommand
 PACKAGE_TYPE = 'pg-tools'
 PACKAGE_NAME = 'pg-export'
 PACKAGE_DESC = 'pg to git converter'
-PACKAGE_VERSION = '3.7.3'
+PACKAGE_VERSION = '3.7.4'
 
 
 class PyTest(TestCommand):
